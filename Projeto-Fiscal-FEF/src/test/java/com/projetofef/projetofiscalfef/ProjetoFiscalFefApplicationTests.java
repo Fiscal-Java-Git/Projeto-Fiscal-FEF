9dd31310054ec47b67dce15d7f7748fceee66dca
DevGabriel-Git <gabriel-spereira@hotmail.com>
2025-10-23 21:29:38 -0300

    Criacao do Projeto Financeiro Academico

---
+++ Projeto-Fiscal-FEF/src/test/java/com/projetofef/projetofiscalfef/ProjetoFiscalFefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofef.projetofiscalfef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFiscalFefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
